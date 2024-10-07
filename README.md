95c040d1137a3cf6adff85d216e38c22cc7622b8
prrockzed <priyanshuranjan2806@gmail.com>
2024-10-07 19:30:44 +0530

    Added an option to backup or delete their existing configuration(if any)
    Not removing the .git folder from now on to get updates

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Installer
-Automatically installs GarudaNvim with one command
+Automatically installs GarudaNvim with a single command
+</br>
+Run the following command to install GarudaNvim:
+```
+wget -q https://raw.githubusercontent.com/GarudaNvim/Installer/main/installGarudaNvim.sh
+chmod +x installGarudaNvim.sh && ./installGarudaNvim.sh
+rm installGarudaNvim.sh
+```
--- installGarudaNvim.sh
+++ installGarudaNvim.sh
@@ -4,51 +4,75 @@
 
 # Welcome message
 echo
+echo
 echo "Welcome to GarudaNvim!"
 echo "This installer is designed for macOS."
 echo
-echo "================================================================================="
+echo "=================================================================================================================="
 echo
 
 # Step 1: Check for existing Neovim configuration
 echo "Step 1: Checking for existing Neovim configuration in ~/.config/nvim"
-echo "---------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------"
 if [ -d ~/.config/nvim ]; then
-  echo "ERROR: The directory ~/.config/nvim already exists."
-  echo "       Please move it to a different location and try again."
+  echo "WARNING: The directory ~/.config/nvim already exists."
+  echo "       You have two options:"
+  echo "       1. Delete your current Neovim configuration and install GarudaNvim."
+  echo "       2. Backup your current Neovim configuration before installing GarudaNvim."
   echo
-  exit 1
+  read -p "Would you like to delete the existing configuration? (y/n): " delete_choice
+  if [ "$delete_choice" = "y" ]; then
+    echo "INFO: Deleting existing Neovim configuration..."
+    # Deleting current configuration
+    rm -rf ~/.config/nvim
+    rm -rf ~/.local/share/nvim
+    rm -rf ~/.local/state/nvim
+    rm -rf ~/.cache/nvim
+    echo "INFO: Existing configuration deleted."
+  else
+    echo
+    echo "Step 1.5: Backup Options"
+    echo "------------------------------------------------------------------------------------------------------------------"
+    echo "You can either automatically backup your current Neovim configuration or manually do it."
+    read -p "Would you like to automatically backup and proceed with the installation? (y/n): " backup_choice
+    if [ "$backup_choice" = "y" ]; then
+      echo "INFO: Backing up current Neovim configuration..."
+      # Automatically backup the current configuration
+      mv ~/.config/nvim{,.bak}
+      # Optional but recommended backups
+      mv ~/.local/share/nvim{,.bak}
+      mv ~/.local/state/nvim{,.bak}
+      mv ~/.cache/nvim{,.bak}
+      echo "INFO: Backup complete!"
+    else
+      echo "INFO: Please manually backup your configuration and try installing GarudaNvim again."
+      echo
+      exit 1
+    fi
+  fi
+else
+  echo "INFO: No existing Neovim configuration found."
 fi
-echo "INFO: No existing Neovim configuration found."
 echo
 echo
 
 # Step 2: Cloning GarudaNvim repository
 echo "Step 2: Installing GarudaNvim to ~/.config/nvim"
-echo "---------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------------------------------"
 git clone https://github.com/garudanvim/GarudaNvim.git ~/.config/nvim
 cd ~/.config/nvim || exit 1
 echo
-echo
-
-# Step 3: Removing the .git folder
-echo "Step 3: Cleaning up"
-echo "---------------------------------------------------------------------------------"
-rm -rf .git
-echo ".git folder removed."
-echo
-echo "================================================================================="
+echo "=================================================================================================================="
 echo
 
 # Success message
 echo "SUCCESS: GarudaNvim has been correctly installed!"
 echo "Happy Coding!"
 echo
-echo "================================================================================="
+echo "=================================================================================================================="
 echo
 
 # Opening GarudaNvim
 echo "PRESS ENTER TO OPEN IT"
 read -r
 nvim
-
